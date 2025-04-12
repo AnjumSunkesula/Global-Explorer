4066cbb0662c947bae8a607d7c141b1bce111001
afroz <afrozdd7@gmail.com>
2025-04-12 15:55:35 +0530

    created login page to let user login to their account

---
+++ src/components/login.jsx
@@ -0,0 +1,10 @@
+function Login () {
+  return(
+    <>
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita, velit!
+      </div>
+    </>
+  );
+}
+export default Login;
