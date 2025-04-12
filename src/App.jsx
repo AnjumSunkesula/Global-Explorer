909f64d2912292fce9c26ead5a91c08f4254cfdf
afroz <afrozdd7@gmail.com>
2025-04-12 19:37:15 +0530

    implemented toggle mode button

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import WelcomePage from "./components/welcome"
 import Login from "./components/login";
-
-
+import { useState,useEffect } from 'react';
+import './App.css'
+// import "./index.css"
 function App() {
+
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    document.documentElement.classList.toggle('dark-mode', darkMode);
+  }, [darkMode]);
+
+  const toggleTheme = () => {
+    setDarkMode(prev => !prev);
+  };
 
   return (
     <>
+       <button onClick={toggleTheme} className="theme-toggle-button">
+        {darkMode ? '☀️ Light Mode' : '🌙 Dark Mode'}
+      </button>
       <Router>
         <Routes>
           <Route exact path='/' Component={WelcomePage}/>
           <Route path='/login' Component={Login} />  
-
-
-
         </Routes>
       </Router>
     </>
