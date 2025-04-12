6aa8190273b4b8352d6eb463c3d878e233802d15
afroz <afrozdd7@gmail.com>
2025-04-12 19:11:27 +0530

    created globeAnimation component

---
+++ src/components/lottieglobe.jsx
@@ -0,0 +1,14 @@
+// src/components/LottieGlobe.jsx
+import React from 'react';
+import Lottie from 'lottie-react';
+import globeAnimation from '../animations/Animation - globe.json'; // Adjust the path if necessary
+
+function LottieGlobe() {
+  return (
+    <div style={{ width: '150px', height: '150px' }}>
+      <Lottie animationData={globeAnimation} loop={true} />
+    </div>
+  );
+}
+
+export default LottieGlobe;
