1425f7c5ffa33dcade5c08317dba02a12b0b4ca4
afroz <afrozdd7@gmail.com>
2025-04-13 19:28:33 +0530

    imported home,countrydetails, savedcountries etc and added routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,15 @@ import WelcomePage from "./components/welcome"
 import Login from "./components/login";
 import { useState,useEffect } from 'react';
 import './App.css'
+import Home from './pages/home';
+import CountryDetails from "./components/CountryDetails";
+import SavedCountries from "./components/SavedCountries";
+import NotFound from "./pages/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute"; // NEW
+import Navbar from './components/navbar';
+
+
+
 // import "./index.css"
 function App() {
 
@@ -26,6 +35,24 @@ function App() {
         <Routes>
           <Route exact path='/' Component={WelcomePage}/>
           <Route path='/login' Component={Login} />  
+
+          <Route path="/home" element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          } />
+          <Route path="/country/:code" element={
+            <ProtectedRoute>
+              <CountryDetails />
+            </ProtectedRoute>
+          } />
+          <Route path="/saved" element={
+            <ProtectedRoute>
+              <SavedCountries />
+            </ProtectedRoute>
+          } />
+
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
     </>
