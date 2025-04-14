bc26aac66a9eb30d100d6b45d2bfd0865153b2d9
afroz <afrozdd7@gmail.com>
2025-04-14 20:26:48 +0530

    modified the display of the links for countries

--- src/components/CountrySearch.jsx
+++ src/components/CountrySearch.jsx
@@ -20,22 +20,62 @@ function CountrySearch() {
   );
 
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="Search for a country"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-      />
-      <ul>
-        {filteredCountries.map((country) => (
-          <li key={country.cca3}>
-            <Link to={`/country/${country.cca3}`}>
-              {country.name.common}
-            </Link>
-          </li>
-        ))}
-      </ul>
+     <div className="search-container overflow-x-hidden">
+      {/* Search Input */}
+      <div className="mb-4">
+        <input
+          type="text"
+          className="form-control form-control-lg"
+          placeholder="Search for a country..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+
+      {/* Scrollable Grid */}
+      <div
+        className="row overflow-auto px-2"
+        style={{ maxHeight: '500px', gap: '1rem' }}
+      >
+        {filteredCountries.length > 0 ? (
+          filteredCountries.map((country) => (
+            <div
+              key={country.cca3}
+              className="col-sm-6 col-md-4 col-lg-3 mb-3"
+            >
+              <div className="card shadow-sm h-100">
+                <img
+                  src={country.flags.png}
+                  className="card-img-top"
+                  alt={`${country.name.common} flag`}
+                  style={{ height: '160px', objectFit: 'cover' }}
+                />
+                <div className="card-body text-center d-flex flex-column justify-content-between">
+                  <h5 className="card-title">{country.name.common}</h5>
+                  <div className="d-flex justify-content-center gap-2 mt-2">
+                    <Link
+                      to={`/country/${country.cca3}`}
+                      className="btn btn-outline-primary btn-sm"
+                    >
+                      View Details
+                    </Link>
+                    <a
+                      href={`https://en.wikipedia.org/wiki/${country.name.common}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="btn btn-outline-secondary btn-sm"
+                    >
+                      Learn More
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p className="text-center col-12">No countries found</p>
+        )}
+      </div>
     </div>
   );
 }
