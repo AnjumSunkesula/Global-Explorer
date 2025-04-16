d24936d7dc53b5698d5430fba675c0ba0dabadae
afroz <afrozdd7@gmail.com>
2025-04-16 13:13:12 +0530

    modified tab spacing in navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,43 +1,40 @@
-// src/components/Navbar.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+function Navbar () {
   return (
     <nav className="navbar navbar-expand-lg fixed-top">
-  <div className="container-fluid">
-    <Link className="navbar-brand" to="/">Global Explorer</Link>
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">Global Explorer</Link>
 
-    {/* Offcanvas Toggle Button */}
-    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-      <span className="navbar-toggler-icon"></span>
-    </button>
+        {/* Offcanvas Toggle Button */}
+        <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-    {/* Offcanvas Menu */}
-    <div className="offcanvas offcanvas-end custom-offcanvas" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-      <div className="offcanvas-header">
-        <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
-        <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        {/* Offcanvas Menu */}
+        <div className="offcanvas offcanvas-end custom-offcanvas" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+          <div className="offcanvas-header">
+            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
+            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div className="offcanvas-body">
+            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/home">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/saved">Saved Countries</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/login">Login</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
-      <div className="offcanvas-body">
-        <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-          <li className="nav-item">
-            <Link className="nav-link active" aria-current="page" to="/home">Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/saved">Saved Countries</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/login">Login</Link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </div>
-   <div className="custom-offcanvas-backdrop"></div>
-</nav>
-
+      <div className="custom-offcanvas-backdrop"></div>
+    </nav>
   );
 };
-
 export default Navbar;
